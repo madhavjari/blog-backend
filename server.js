031f87bf278c25ae16b1f0fcf47063a02b1ef422
madhavjari <213913746+madhavjari@users.noreply.github.com>
2026-06-13 18:56:59 +0530

    bug:  inside origin removed it

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://blog-frontend-ten-theta.vercel.app/",
+      "https://blog-frontend-ten-theta.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
