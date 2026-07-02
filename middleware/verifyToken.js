f7bbe13bc9b78a8105693c2b907579de13e6a0e0
madhavjari <213913746+madhavjari@users.noreply.github.com>
2026-07-02 18:40:57 +0530

    bug: no token would have left with server hanging, changed if condition

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -3,21 +3,22 @@ const { findUser } = require("../db/queries");
 
 async function verifyToken(req, res, next) {
   const bearerHeader = req.headers["authorization"];
-  if (bearerHeader && bearerHeader.startsWith("Bearer ")) {
-    const bearer = bearerHeader.split(" ");
-    const bearerToken = bearer[1];
-    try {
-      const payload = jwt.verify(bearerToken, process.env.JWT_SECRET_KEY, {
-        algorithms: ["HS256"],
-        issuer: process.env.JWT_ISSUER,
-        audience: process.env.JWT_AUDIENCE,
-      });
-      const user = await findUser(payload.sub);
-      req.user = user;
-      next();
-    } catch {
-      res.status(401).json({ message: "Wrong token" });
-    }
+  if (!bearerHeader || !bearerHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+  const bearer = bearerHeader.split(" ");
+  const bearerToken = bearer[1];
+  try {
+    const payload = jwt.verify(bearerToken, process.env.JWT_SECRET_KEY, {
+      algorithms: ["HS256"],
+      issuer: process.env.JWT_ISSUER,
+      audience: process.env.JWT_AUDIENCE,
+    });
+    const user = await findUser(payload.sub);
+    req.user = user;
+    next();
+  } catch {
+    res.status(401).json({ message: "Wrong token" });
   }
 }
 
